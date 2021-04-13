d1e447d622480f1e75fdf43126d5becff2174801
Troublesm1 <Jovahnn@yahoo.com>
2021-04-12 22:15:23 -0400

    First upload Of Cookie Order

--- src/main/java/cookies/CookieOrder.java
+++ src/main/java/cookies/CookieOrder.java
@@ -1,4 +1,13 @@
 package cookies;
 
 public class CookieOrder {
+
+    private int numBoxes;
+
+    public CookieOrder(String variety, int localNumBoxes) {
+    }
+
+    public int getNumBoxes() {
+        return numBoxes;
+    }
 }
--- src/main/java/cookies/MasterOrder.java
+++ src/main/java/cookies/MasterOrder.java
@@ -1,4 +1,18 @@
 package cookies;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MasterOrder {
+    List<CookieOrder> orders = new ArrayList<>();
+    public void addOrder(CookieOrder cookieOrder) {
+        orders.add(cookieOrder);
+    }
+    public int getTotalBoxes() {
+        int boxes =0;
+        for(CookieOrder order: orders){
+            boxes += order.getNumBoxes();
+        }
+        return boxes;
+    }
 }
