a6ba105eb7db5ad31cdc8e79cb694f6d65f43f7c
Troublesm1 <Jovahnn@yahoo.com>
2021-04-14 21:12:12 -0400

    Final Build

--- src/main/java/cookies/OrderApp.java
+++ src/main/java/cookies/OrderApp.java
@@ -1,9 +1,12 @@
 package cookies;
+import java.util.Scanner;
 
 public class OrderApp {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
         MasterOrder myOrder = new MasterOrder();
+
         myOrder.addOrder(new CookieOrder("Tagalongs", 1));
         myOrder.addOrder(new CookieOrder("Thin Mints", 5));
         myOrder.addOrder(new CookieOrder("Samoas", 2));
@@ -11,8 +14,18 @@ public class OrderApp {
 
         System.out.println("Current Order");
         myOrder.showOrder();
+        System.out.println();
         System.out.println("You have ordered " + myOrder.getTotalBoxes() + " boxes");
-        
+        System.out.println("Current Order");
+        System.out.println();
+        System.out.println("What would you like to remove? (specify a variety or none)");
+
+        String varietyToRemove = input.nextLine();
+        System.out.println("You are removing " + myOrder.getVarietyBoxes(varietyToRemove) + " " + varietyToRemove);
+        myOrder.removeVariety(varietyToRemove);
+        System.out.println("Current Order");
+        myOrder.showOrder();
+
 
     }
 }
